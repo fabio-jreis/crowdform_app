6d0057a5740e23782bdd94d69102e306fd9bbb32
Fabio Reis <fj.reis@gmail.com>
2023-06-16 05:44:50 -0700

    Added Login page, NewAccount page and react navigation

---
+++ src/NewAccount.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+export default function NewAccount() {
+    return(
+        <View>
+            <Text>New Account</Text>
+        </View>
+    )
+}
