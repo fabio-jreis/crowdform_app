049c3267a2c92f302d69127a24789c192e24e2d2
Fabio Reis <fj.reis@gmail.com>
2023-06-17 08:23:37 -0700

    Added some infos in funds details page

--- src/AppContent/FundsDetails.js
+++ src/AppContent/FundsDetails.js
@@ -1,5 +1,10 @@
+// Asset details
+// - Screen to present details of the selected asset/fund. 
+// The layout is an illustration of the information that should appear. 
+// Use your creativity and you can use images of your choice to compose the details
+
 import React, { useState } from 'react'
-import { Text } from "react-native"
+import { Text, View, StyleSheet } from "react-native"
 import { useSelector } from 'react-redux'
 import {useRoute} from "@react-navigation/native"
 
@@ -10,6 +15,87 @@ export default function FundsDetails() {
     const fundDetail = funds[index];
 
     return(
-        <Text>{fundDetail.title}</Text>
+        <View style={styles.contentHome}>
+            <Text style={styles.fundsTitle}>{fundDetail.title}</Text>
+
+            <Text style={styles.info}>Info & Status</Text>
+            <View>
+                <View style={{flexDirection: "row",  marginBottom: 18, marginTop: 20}}>
+                    <View style={{flex: 1}}>
+                        <Text style={styles.infoDetailsLabel}>AUM</Text>
+                        <Text>{fundDetail.aum}</Text>
+                    </View>
+                    <View style={{flex: 1}}>
+                        <Text style={styles.infoDetailsLabel}>Issue Date</Text>
+                        <Text>{fundDetail.issueDate}</Text>
+                    </View>
+                </View>
+                <View style={{flexDirection: "row", marginBottom: 18}}>
+                    <View style={{flex: 1}}>
+                        <Text style={styles.infoDetailsLabel}>Vintage Range</Text>
+                        <Text>{fundDetail.vintageRange}</Text>
+                    </View>
+                    <View style={{flex: 1}}>
+                        <Text style={styles.infoDetailsLabel}>TER</Text>
+                        <Text>{fundDetail.ter}</Text>
+                    </View>
+                </View>
+                <View style={{flexDirection: "row"}}>
+                    <View style={{flex: 1}}>
+                        <Text style={styles.infoDetailsLabel}>Price at Close</Text>
+                        <Text>{fundDetail.priceAtCost}</Text>
+                    </View>
+                    <View style={{flex: 1}}>
+                        <Text style={styles.infoDetailsLabel}>Price at Open</Text>
+                        <Text>{fundDetail.priceAtOpen}</Text>
+                    </View>
+                </View>
+            </View>
+        </View>
     )
 }
+
+const styles = StyleSheet.create({
+    fundsTitle: {
+        fontFamily: 'Sora',
+        fontStyle: 'normal',
+        fontWeight: 600,
+        fontSize: 18,
+        lineHeight: 23,
+        display: 'flex',
+        alignItems: 'center',
+        letterSpacing: -0.02,
+        color: '#000000'   
+    },
+    contentHome: {
+        marginTop: 55,
+        marginLeft: 20,
+        marginRight: 20
+    },
+    info: {
+        marginTop: 20,
+        fontFamily: 'Sora',
+        fontStyle: 'normal',
+        fontWeight: 600,
+        fontSize: 17,
+        lineHeight: 21,
+        letterSpacing: -0.02,
+        color: '#000000' 
+    },
+    infoDetailsLabel: {
+        fontFamily: 'Sora',
+        fontStyle: 'normal',
+        fontWeight: 400,
+        fontSize: 14,
+        letterSpacing: -0.02,     
+        color: '#A0A0A0'   
+    },
+    infoResults: {
+        fontFamily: 'Sora',
+        fontStyle: 'normal',
+        fontWeight: 400,
+        fontSize: '14px',
+        letterSpacing: '-0.02',        
+        color: '#000000'       
+    }
+})
--- src/reducer/FundsReducer.js
+++ src/reducer/FundsReducer.js
@@ -5,22 +5,39 @@ const initialState = {
             title: "Wind Fund",
             icon: "Wind",
             price: "$1032.23",
-            percentage: "3.51%"
-
+            percentage: "3.51%",
+            aum: "$430.88m",
+            issueDate: "18/04/2022",
+            vintageRange: "2019-2022",
+            ter: "0.15%",
+            priceAtCost: "$17.68",
+            priceAtOpen: "$17.74"
         },
         {
             key: 2,
             title: "Solar Fund",
             icon: "Sun",
             price: "$986.61",
-            percentage: "0.13%"
+            percentage: "0.13%",
+            aum: "$200.70m",
+            issueDate: "13/01/2022",
+            vintageRange: "2020-2022",
+            ter: "0.20%",
+            priceAtCost: "$18.61",
+            priceAtOpen: "$18.34"            
         },
         {
             key: 3,
             title: "Nature Fund",
             icon: "Narute",
             price: "$1122.95",
-            percentage: "5.00%"
+            percentage: "5.00%",
+            aum: "$109.38m",
+            issueDate: "01/01/2022",
+            vintageRange: "2021-2022",
+            ter: "2.10%",
+            priceAtCost: "$50.15",
+            priceAtOpen: "$12.00"            
         }
     ]
 }
